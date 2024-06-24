1da1122967e82fbd80e858e7e1d9babf869145c9
zbytes2227 <zbytes0@gmail.com>
2024-06-24 22:57:41 +0530

    final gwy

--- app/student/page.js
+++ app/student/page.js
@@ -40,6 +40,7 @@ const Page = () => {
     const data = await fetch_api.json();
     if (data.success) {
       console.log(data);
+      setCustomerName(data.Names)
       setUpdate(data.Updates)
     }
   };
@@ -49,26 +50,29 @@ const Page = () => {
   return (
     <>
       <div className="mt-20">
-        <h2 className="mb-5 text-2xl font-bold text-center">
+        <h2 className="mb-10 text-3xl font-bold text-center">
           Student Token  {CustomerID} Status
         </h2>
       </div>
       <div class="max-w-lg text-center mx-auto border border-3 rounded-lg p-5">
-      <div>
-      {Update.map((update) => (
-        <div key={update._id} className="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-blue-50" role="alert">
-          <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-          </svg>
-          <span className="sr-only">Info</span>
-          <div>
-            <span className="font-medium">{update.update}</span> 
-          </div>
+        <div>
+        <h2 className="mb-5 text-2xl font-semibold text-center">
+          Student Name :  {CustomerName}
+        </h2>
+          {Update.map((update) => (
+            <div key={update._id} className="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-blue-50" role="alert">
+              <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span className="sr-only">Info</span>
+              <div>
+                <span className="font-medium">{update.update}</span>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-    
 
+    
         {/* <div class="flex items-start mb-5">
     <div class="flex items-center h-5">
       <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required/>
@@ -78,7 +82,7 @@ const Page = () => {
         <button
           class="mt-20 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
         >
-        Wait for counselling to Start --  SONU SIR CLASSES
+          Wait for counselling to Start --  SONU SIR CLASSES
         </button>
       </div>
     </>
--- pages/api/getUpdate.js
+++ pages/api/getUpdate.js
@@ -2,6 +2,7 @@
 import Update from "@/model/Updates";
 import connectDb from "../../middleware/mongoose";
 import { parse } from "cookie"; import jwt from "jsonwebtoken";
+import Customers from "@/model/Customers";
 
 const handler = async (req, res) => {
 if (req.method === "POST") {
@@ -9,9 +10,11 @@ if (req.method === "POST") {
 
       // Find all cards in the database
       const allCards = await Update.find({CustomerID: req.body.id});
+      const student = await Customers.find({CustomerID: req.body.id});
+  
 
       // Return the found cards as a JSON response
-      return res.status(200).json({ success: true, Updates: allCards });
+      return res.status(200).json({ success: true, Updates: allCards, Names : student[0].CustomerName });
     } catch (err) {
       console.error(err);
       return res.status(500).json({ success: false, msg: "Server error. Contact the Developers." });
