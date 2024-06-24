d741e565247fda406f8e3e4e66a29b0197c9d708
zbytes2227 <zbytes0@gmail.com>
2024-06-24 19:34:17 +0530

    first commit

--- model/Customers.js
+++ model/Customers.js
@@ -5,8 +5,8 @@ const CustomerSchema = new mongoose.Schema(
   {
     CustomerID: { type: String, required: true, unique: true },
     CustomerName: { type: String },
-    CustomerWatsapp: { type: String, required: true, unique: true },
     CustomerPhone: { type: String, required: true, unique: true },
+    CustomerWatsapp: { type: String, required: true, unique: true },
     CustomerTransaction: { type: String, required: true, unique: true },
   },
   { collection: "all-Customers" },
